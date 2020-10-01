5c8da728d5275967a2c5e60fc60563bc6a00a2f9
Chen Lijun <chenlijun1999@gmail.com>
2020-10-01 16:39:45 +0200

    i3wm: fix and fine tune

--- src/config/i3/scripts/lock.sh
+++ src/config/i3/scripts/lock.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-IMG_DIR="/tmp/chenlijun99/scripts/lock/"
+IMG_DIR="/tmp/chenlijun99/scripts/lock"
 IMG_PATH="${IMG_DIR}/screen.png"
 
 mkdir -p "${IMG_DIR}"
+rm -f ${IMG_DIR}/*
+
+# Wait for rofi menu to close
+sleep 0.2
 
 scrot "${IMG_PATH}"
 convert "${IMG_PATH}" -scale 10% -blur 0x2.5 -resize 1000% "${IMG_PATH}"
