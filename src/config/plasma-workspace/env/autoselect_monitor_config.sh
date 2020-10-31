def82b5db4c838706f6786c014cc480df0cf39ef
Chen Lijun <chenlijun1999@gmail.com>
2020-11-01 00:00:30 +0100

    autorandr: minor improvements

--- src/config/plasma-workspace/env/autoselect_monitor_config.sh
+++ src/config/plasma-workspace/env/autoselect_monitor_config.sh
@@ -1,1 +1,3 @@
-/home/lijun/Repositories/personal/dotfiles/src/config/rofi/scripts/config_monitors/autoselect_monitor_config.sh
+#!/usr/bin/env bash
+
+~/.config/rofi/scripts/config_monitors/autoselect_monitor_config.sh
