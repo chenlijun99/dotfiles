dc33b50384b6efacd4273e8a3a7504f902472b79
Lijun Chen <chenlijun1999@gmail.com>
2022-03-06 17:55:57 +0100

    vim: properly format lua files with stylua

--- src/vim/config/plugin/tree-sitter.lua
+++ src/vim/config/plugin/tree-sitter.lua
@@ -1,51 +1,56 @@
-Plug('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate', config = function()
-	require'nvim-treesitter.configs'.setup {
-		ensure_installed = "maintained",
-		highlight = {
-			enable = true,
-			disable = {
-				"vim",
-				-- typescript is very slow with treesitter
-				"typescript",
-				"tsx",
-				"tex",
-				"cmake"
+Plug("nvim-treesitter/nvim-treesitter", {
+	run = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = "maintained",
+			highlight = {
+				enable = true,
+				disable = {
+					"vim",
+					-- typescript is very slow with treesitter
+					"typescript",
+					"tsx",
+					"tex",
+					"cmake",
+				},
 			},
-		},
-		indent = {
-			enable = true,
-			disable = {
-				"vim",
-				-- typescript is very slow with treesitter
-				"typescript",
-				"tsx",
-				"tex",
-				"cmake"
+			indent = {
+				enable = true,
+				disable = {
+					"vim",
+					-- typescript is very slow with treesitter
+					"typescript",
+					"tsx",
+					"tex",
+					"cmake",
+				},
 			},
-		},
-	}
-end
+		})
+	end,
 })
-Plug('nvim-treesitter/playground', { config = function()
-	require "nvim-treesitter.configs".setup {
-		playground = {
-			enable = true,
-			disable = {},
-			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-			persist_queries = false, -- Whether the query persists across vim sessions
-			keybindings = {
-				toggle_query_editor = 'o',
-				toggle_hl_groups = 'i',
-				toggle_injected_languages = 't',
-				toggle_anonymous_nodes = 'a',
-				toggle_language_display = 'I',
-				focus_language = 'f',
-				unfocus_language = 'F',
-				update = 'R',
-				goto_node = '<cr>',
-				show_help = '?',
+Plug("nvim-treesitter/playground", {
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			playground = {
+				enable = true,
+				disable = {},
+				-- Debounced time for highlighting nodes in the playground from source code
+				updatetime = 25,
+				-- Whether the query persists across vim sessions
+				persist_queries = false,
+				keybindings = {
+					toggle_query_editor = "o",
+					toggle_hl_groups = "i",
+					toggle_injected_languages = "t",
+					toggle_anonymous_nodes = "a",
+					toggle_language_display = "I",
+					focus_language = "f",
+					unfocus_language = "F",
+					update = "R",
+					goto_node = "<cr>",
+					show_help = "?",
+				},
 			},
-		}
-	}
-	end
+		})
+	end,
 })
