1a5530ebd84836c95c237d57afe2b0e8eef52028
Lijun Chen <chenlijun1999@gmail.com>
2022-03-27 12:00:33 +0200

    vim: use alejandra as nix formatter

--- src/vim/config/plugin/null-ls.lua
+++ src/vim/config/plugin/null-ls.lua
@@ -2,9 +2,30 @@ Plug("nvim-lua/plenary.nvim")
 Plug("jose-elias-alvarez/null-ls.nvim", {
 	config = function()
 		local null_ls = require("null-ls")
+		local h = require("null-ls.helpers")
+		local methods = require("null-ls.methods")
+
+		local FORMATTING = methods.internal.FORMATTING
+
+		alejandra = h.make_builtin({
+			name = "alejandra",
+			meta = {
+				url = "https://github.com/kamadorueda/alejandra",
+				description = " The Uncompromising Nix Code Formatter ",
+			},
+			method = FORMATTING,
+			filetypes = { "nix" },
+			generator_opts = {
+				command = "alejandra",
+				to_stdin = true,
+			},
+			factory = h.formatter_factory,
+		})
+
 		null_ls.setup({
 			sources = {
 				-- formatters
+				alejandra,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.clang_format,
