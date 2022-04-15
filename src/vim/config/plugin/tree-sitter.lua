ffe9f6bfa281140ee8627019c8a9f6f43785eb94
Lijun Chen <chenlijun1999@gmail.com>
2022-04-15 13:45:55 +0200

    vim: fix deprecated nvim-treesitter config

--- src/vim/config/plugin/tree-sitter.lua
+++ src/vim/config/plugin/tree-sitter.lua
@@ -2,7 +2,7 @@ Plug("nvim-treesitter/nvim-treesitter", {
 	run = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = "maintained",
+			ensure_installed = "all",
 			highlight = {
 				enable = true,
 				disable = {
