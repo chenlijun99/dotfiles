40387c762a30714c725b9b8399d4c114c0da798e
Lijun Chen <chenlijun1999@gmail.com>
2022-04-17 13:09:24 +0000

    nixos: add more configurations

---
+++ src/nixos/modules/docker.nix
@@ -0,0 +1,6 @@
+#
+# System-wide configuration related to Docker
+#
+{...}: {
+  virtualisation.docker.enable = true;
+}
--- src/nixos/users/lijun/default.nix
+++ src/nixos/users/lijun/default.nix
@@ -4,7 +4,7 @@
     lijun = {
       isNormalUser = true;
       description = "Lijun Chen";
-      extraGroups = ["wheel" "networkmanager"];
+      extraGroups = ["wheel" "networkmanager" "docker"];
       hashedPassword = "$6$SUUT8ZWKpI$3/0xAo2JFFOmtBPxSwGGzKdMgD5slbPaZgHWd9l53SELl8ohaMAVDeIiY6E15LXG0Lmqc1wDKSFjM7f/cMArQ.";
     };
   };
