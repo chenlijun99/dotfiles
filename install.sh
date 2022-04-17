118065e7ec85c5f77244cbbb62f6a651d4d678ef
Lijun Chen <chenlijun1999@gmail.com>
2022-04-17 18:54:25 +0200

    nixos: configure input method (based on ibus)

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ function main()
 		# ensure that $BACKUP_DIR exists
 		mkdir -p "$BACKUP_DIR"
 		install "$SCRIPT_DIR/src" "$HOME" "." "^(config|system|local|nixos)"
-		install "$SCRIPT_DIR/src/config" "$HOME/.config"
+		install "$SCRIPT_DIR/src/config" "$HOME/.config" "" "^(ibus)"
+		install "$SCRIPT_DIR/src/config/ibus/rime/" "$HOME/.config/ibus/rime"
 		install "$SCRIPT_DIR/src/local/bin" "$HOME/.local/bin/"
 		install "$SCRIPT_DIR/src/local/share/applications" "$HOME/.local/share/applications/"
 
