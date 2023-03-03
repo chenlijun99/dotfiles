590e5d25e346e07381d0f5ad13a8c31b176079ce
Lijun Chen <chenlijun1999@gmail.com>
2023-03-03 18:26:57 +0100

    nixos: update flake

--- src/nixos/flake.nix
+++ src/nixos/flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manage = {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
