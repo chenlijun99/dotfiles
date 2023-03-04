78151ada2516bfa8f05d4c737233587ddf09ab66
Lijun Chen <chenlijun1999@gmail.com>
2023-03-04 22:48:30 +0100

    nixos: add more search engine aliases for firefox

--- src/nixos/users/common/lijun-base/firefox.nix
+++ src/nixos/users/common/lijun-base/firefox.nix
@@ -26,6 +26,14 @@
           icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
           definedAliases = ["np"];
         };
+        "NixOS options" = {
+          urls = [
+            {
+              template = "https://search.nixos.org/options?query={searchTerms}";
+            }
+          ];
+          definedAliases = ["no"];
+        };
         "NixOS Wiki" = {
           urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
           iconUpdateURL = "https://nixos.wiki/favicon.png";
