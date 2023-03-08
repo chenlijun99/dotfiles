05f426b8e35f892c2be2e2671114490b2f06e134
Lijun Chen <chenlijun1999@gmail.com>
2023-03-08 17:32:17 +0100

    nixos: add gnome-disks

--- src/nixos/modules/base-gui.nix
+++ src/nixos/modules/base-gui.nix
@@ -9,6 +9,7 @@
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     gparted
+    gnome.gnome-disk-utility
     # Main browser that all the users can use
     # This is a prebuilt binary from Mozilla.
     # NixOS provides also "firefox", which is built using Nix.
