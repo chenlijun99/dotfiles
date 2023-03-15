7e0a8e2b4bba5b6795c74fec3084472ecefe5de2
Lijun Chen <chenlijun1999@gmail.com>
2023-03-15 01:02:40 +0100

    nixos: use custom okular

--- src/nixos/users/lijun/home.nix
+++ src/nixos/users/lijun/home.nix
@@ -64,6 +64,10 @@
       # Note taking app
       obsidian
 
+      (libsForQt5.okular.overrideAttrs (oldAttrs: rec {
+        patches = (lib.optionals (oldAttrs ? patches) oldAttrs.patches) ++ [./okular.patch];
+      }))
+
       # Email client
       thunderbird
 
