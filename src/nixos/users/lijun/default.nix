263363d813fea195cc7ede354034beb2c3b8e1f1
Lijun Chen <chenlijun1999@gmail.com>
2023-03-17 22:44:51 +0100

    nixos: add `lijun` user to the `dialout` group

--- src/nixos/users/lijun/default.nix
+++ src/nixos/users/lijun/default.nix
@@ -4,7 +4,7 @@
     lijun = {
       isNormalUser = true;
       description = "Lijun Chen";
-      extraGroups = ["wheel" "networkmanager" "docker"];
+      extraGroups = ["wheel" "networkmanager" "docker" "dialout"];
       # Initial password.
       # Change it immediately on the first login!
       hashedPassword = "$6$SUUT8ZWKpI$3/0xAo2JFFOmtBPxSwGGzKdMgD5slbPaZgHWd9l53SELl8ohaMAVDeIiY6E15LXG0Lmqc1wDKSFjM7f/cMArQ.";
