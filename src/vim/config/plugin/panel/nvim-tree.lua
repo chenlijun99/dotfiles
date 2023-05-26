2115037faba3050fa55affe23b4197eb184d1d5d
Lijun Chen <chenlijun1999@gmail.com>
2023-05-26 18:31:07 +0200

    vim: upgrade plugins
    * nvim-tree: deal with deprecated config

--- src/vim/config/plugin/panel/nvim-tree.lua
+++ src/vim/config/plugin/panel/nvim-tree.lua
@@ -12,6 +12,67 @@ function NvimTreeOpenOrFindFile()
 	end
 end
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings not inserted as:
+  --  remove_keymaps = true
+  --  OR
+  --  view.mappings.custom_only = true
+
+
+  -- Mappings migrated from view.mappings.list
+  --
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
+  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
+  vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
+  vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
+  vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+  vim.keymap.set('n', 'x', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+  vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
+  vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
+  vim.keymap.set('n', 'i', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+  vim.keymap.set('n', 'I', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', '<c-l>', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'ma', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'md', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'mD', api.fs.trash, opts('Trash'))
+  vim.keymap.set('n', 'mr', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', 'mR', api.fs.rename_sub, opts('Rename: Omit Filename'))
+  vim.keymap.set('n', 'mx', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', 'mm', api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'mc', api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'mp', api.fs.paste, opts('Paste'))
+  vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+  vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+  vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+  vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
+  vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
+  vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+  vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+  vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+  vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+  vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
+
+end
+
 return {
 	{
 		"kyazdani42/nvim-tree.lua",
@@ -24,90 +85,8 @@ return {
 			},
 		},
 		config = function()
-			local mappings_list = {
-				{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-				{
-					key = "<C-e>",
-					action = "edit_in_place",
-				},
-				{
-					key = { "O" },
-					action = "edit_no_picker",
-				},
-				{ key = { "<2-RightMouse>", "<C-]>" },    action = "cd" },
-				{ key = "<C-v>",                          action = "vsplit" },
-				{ key = "<C-x>",                          action = "split" },
-				{ key = "<C-t>",                          action = "tabnew" },
-				{
-					key = "<",
-					action = "prev_sibling",
-				},
-				{
-					key = ">",
-					action = "next_sibling",
-				},
-				{
-					key = "P",
-					action = "parent_node",
-				},
-				{
-					key = "x",
-					action = "close_node",
-				},
-				{ key = "<Tab>", action = "preview" },
-				{
-					key = "K",
-					action = "first_sibling",
-				},
-				{ key = "J",     action = "last_sibling" },
-				{
-					key = "i",
-					action = "toggle_ignored",
-				},
-				{
-					key = "I",
-					action = "toggle_dotfiles",
-				},
-				{ key = "<c-l>", action = "refresh" },
-				{ key = "ma",    action = "create" },
-				{ key = "md",    action = "remove" },
-				{ key = "mD",    action = "trash" },
-				{ key = "mr",    action = "rename" },
-				{ key = "mR",    action = "full_rename" },
-				{ key = "mx",    action = "system_open" },
-				{ key = "mm",    action = "cut" },
-				{ key = "mc",    action = "copy" },
-				{ key = "mp",    action = "paste" },
-				{ key = "y",     action = "copy_name" },
-				{ key = "Y",     action = "copy_path" },
-				{
-					key = "gy",
-					action = "copy_absolute_path",
-				},
-				{
-					key = "[c",
-					action = "prev_git_item",
-				},
-				{
-					key = "]c",
-					action = "next_git_item",
-				},
-				{ key = "-",  action = "dir_up" },
-				{ key = "q",  action = "close" },
-				{ key = "g?", action = "toggle_help" },
-				{ key = "W",  action = "collapse_all" },
-				{ key = "S",  action = "search_node" },
-				{
-					key = "<C-k>",
-					action = "toggle_file_info",
-				},
-				{
-					key = ".",
-					action = "run_file_command",
-				},
-			}
-
 			require("nvim-tree").setup({
+				on_attach = on_attach,
 				-- Disable auto refresh on save, since it introduces lags
 				auto_reload_on_write = false,
 				filters = {
@@ -130,7 +109,6 @@ return {
 					width = 25,
 					mappings = {
 						custom_only = true,
-						list = mappings_list,
 					},
 				},
 				renderer = {
