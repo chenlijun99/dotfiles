04d14f5c8f42b7217323c64b377d3a92ac0fbf8f
Lijun Chen <chenlijun1999@gmail.com>
2023-07-13 16:00:00 +0200

    nixos: improve SSH server security

--- src/nixos/modules/ssh-server.nix
+++ src/nixos/modules/ssh-server.nix
@@ -1,3 +1,24 @@
-{...}: {
-  services.openssh.enable = true;
+{ lib, ...}: {
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      # Disable insecure password authentication
+      PasswordAuthentication = false;
+    };
+    extraConfig = ''
+      # Allow
+      # * publickey authentication
+      # * keyboard-interactive authentication for 2fa
+      AuthenticationMethods publickey,keyboard-interactive
+    '';
+  };
+  security.pam.services = {
+    sshd = {
+      googleAuthenticator.enable = true;
+      # Required to make googleAuthenticator work.
+      # See https://github.com/NixOS/nixpkgs/issues/115044#issuecomment-791909238
+      unixAuth = lib.mkForce true;
+    };
+  };
 }
