b03166bc0906e6cd785126af91c8cf1b3061497d
Lijun Chen <chenlijun1999@gmail.com>
2023-07-14 16:06:03 +0000

    nixos: extend lijun-test's "permissions"

--- src/nixos/users/lijun-test/default.nix
+++ src/nixos/users/lijun-test/default.nix
@@ -1,12 +1,15 @@
 # "lijun-test" is a user that has the same development environment to which I
 # am used, but not all the other stuff (note taking, messaging, etc.).
-# Nor has access to sudo.
+# Use cases:
+# * I'm presenting something, I don't want to leak any personal info during 
+# the preseentation (chat notifications, emails, browser bookmarks, browser 
+# history, etc.)
 {...}: {
   users.users = {
     lijun-test = {
       isNormalUser = true;
       description = "Lijun Chen Test";
-      extraGroups = ["networkmanager"];
+      extraGroups = ["wheel" "networkmanager" "docker" "dialout" "libvirtd" "wireshark"];
       # Initial empty password
       hashedPassword = "";
     };
