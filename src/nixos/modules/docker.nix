60d65a962207c540312c20de5bfae0f65ff0c7dd
Lijun Chen <chenlijun1999@gmail.com>
2023-10-20 14:25:50 +0200

    nixos: add xhost

--- src/nixos/modules/docker.nix
+++ src/nixos/modules/docker.nix
@@ -1,6 +1,11 @@
 #
 # System-wide configuration related to Docker
 #
-{...}: {
+{pkgs, ...}: {
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    # Not the most secure option, but `xhost` is useful to remove X11 authorization.
+    # I sometimes use this to mmake GUI from docker container work.
+    xorg.xhost
+  ];
 }
