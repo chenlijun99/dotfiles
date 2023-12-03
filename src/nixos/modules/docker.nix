ea9b45aa4cda5c2499bbc3660750b723a6a88a2a
Lijun Chen <chenlijun1999@gmail.com>
2023-12-03 20:14:07 +0100

    nixos: temporarily use older Docker to workaround problems with OMNET++ IDE

--- src/nixos/modules/docker.nix
+++ src/nixos/modules/docker.nix
@@ -2,7 +2,12 @@
 # System-wide configuration related to Docker
 #
 {pkgs, ...}: {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    # For unknown reasons, OMNET++ IDE in my Docker container doesn't work 
+    # when using Docker 24.0.5.
+    package = pkgs.docker_20_10;
+  };
   environment.systemPackages = with pkgs; [
     # Not the most secure option, but `xhost` is useful to remove X11 authorization.
     # I sometimes use this to mmake GUI from docker container work.
