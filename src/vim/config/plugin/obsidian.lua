41783bfffcb68be707dc95e3db528ec1355b0d21
Lijun Chen <chenlijun1999@gmail.com>
2024-03-05 10:40:08 +0100

    vim: add obsidian plugin

---
+++ src/vim/config/plugin/obsidian.lua
@@ -0,0 +1,30 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "root",
+				path = function()
+					return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+				end,
+			},
+		},
+		-- Optional, boolean or a function that takes a filename and returns a boolean.
+		-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+		disable_frontmatter = true,
+		ui = {
+			-- set to false to disable all additional syntax features
+			enable = false,
+		},
+
+		-- see below for full list of options 👇
+	},
+}
