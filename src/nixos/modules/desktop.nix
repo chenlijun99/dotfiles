2dc05d57c27d20475f7fb562b164e058119fa508
Lijun Chen <chenlijun1999@gmail.com>
2024-06-06 15:23:11 +0200

    nixos: fix warnings
    One thing I did to fix one warning was to bump up `system.stateVersion`.
    In general, it is advised against, as the user has to then manually
    perform data migration. But I don't think I'm using anything (e.g. databases)
    that require such migration. All the data I care about is stored elsewhere.

--- src/nixos/modules/desktop.nix
+++ src/nixos/modules/desktop.nix
@@ -2,15 +2,17 @@
 # System-wide configuration related to desktop environment
 #
 {pkgs, ...}: {
+  services.displayManager = {
+    sddm.enable = true;
+  };
   services.xserver = {
     enable = true;
-    displayManager.sddm.enable = true;
     desktopManager.plasma5 = {
       enable = true;
     };
-    libinput.enable = true; # for touchpad support on many laptops
   };
-
+  # for touchpad support on many laptops
+  services.libinput.enable = true;
   # Fixes https://github.com/NixOS/nixpkgs/issues/145354
   # I need to this open Zotero links
   xdg.portal = {
--- src/nixos/modules/printing.nix
+++ src/nixos/modules/printing.nix
@@ -8,6 +8,6 @@
 {pkgs, ...}: {
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   services.avahi.openFirewall = true;
 }
