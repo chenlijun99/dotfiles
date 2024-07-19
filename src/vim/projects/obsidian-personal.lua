270a63faf8c4360defc8ca1dd0a1ffbeec771224
Lijun Chen <chenlijun1999@gmail.com>
2024-07-19 17:18:59 +0200

    vim: fine tune markdown-oxide config for Obsidian

--- src/vim/projects/obsidian-personal.lua
+++ src/vim/projects/obsidian-personal.lua
@@ -4,7 +4,22 @@ local nvim_lsp = require("lspconfig")
 -- Thus if I open a file in a deep folder from Obsidian, I won't have all this
 -- config.
 
+-- An example nvim-lspconfig capabilities setting
+local capabilities = require("cmp_nvim_lsp").default_capabilities(
+	vim.lsp.protocol.make_client_capabilities()
+)
+
+-- Ensure that dynamicRegistration is enabled! This allows the LS to take into account actions like the
+-- Create Unresolved File code action, resolving completions for unindexed code blocks, ...
+capabilities.workspace = {
+	didChangeWatchedFiles = {
+		dynamicRegistration = true,
+	},
+}
+
 -- Enable markdown_oxide of Obsidian vault
-nvim_lsp.markdown_oxide.setup{}
+nvim_lsp.markdown_oxide.setup({
+	capabilities = capabilities,
+})
 -- Call LspStart to make the newly set up LSP attach to the current buffer
 vim.cmd("LspStart")
