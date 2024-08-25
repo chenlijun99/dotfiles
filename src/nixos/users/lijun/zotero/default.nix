9f8d0f565b59a4cb7596dea7986bdeb05c6b5ece
Lijun Chen <chenlijun1999@gmail.com>
2024-08-25 15:36:07 +0200

    nixos: upgrade
    * Upgraded Zotero 6 -> Zotero 7. ZotFile doesn't work anymore, so moved
      to ZotMoov. Updated config accordingly.

--- src/nixos/users/lijun/zotero/default.nix
+++ src/nixos/users/lijun/zotero/default.nix
@@ -14,6 +14,21 @@
       settings = {
         # Use System PDF reader to open PDF
         "extensions.zotero.fileHandler.pdf" = "system";
+
+        "extensions.zotmoov.attach_search_dir" = "${config.home.homeDirectory}/Downloads";
+        "extensions.zotmoov.dst_dir" = "${config.home.homeDirectory}/pCloudDrive/Zotero";
+        "extensions.zotmoov.enable_attach_dir" = true;
+
+        # This setting from Zotero will be used also by zotmoov when moving files to the destination directory.
+        "extensions.zotero.attachmentRenameTemplate" = "{{ firstCreator suffix=\" - \" }}{{ year suffix=\" - \" }}{{ title truncate=\"80\" }}";
+
+        "extensions.zotero.translators.better-bibtex.citekeyFormat" = "auth.lower+shorttitle(3,3)+year";
+        "extensions.zotero.translators.better-bibtex.citekeyFormatEditing" = "auth.lower+shorttitle(3,3)+year";
+
+        ########################################################################
+        # Below are the ZotFile settings. I don't use ZotFile anymore.
+        # Anyway, I guess it's not harmful to keep them around.
+        ########################################################################
         # Don't copy files into Zotero. Rather, create links to dest_dir.
         "extensions.zotfile.import" = false;
         # Where to move the attachments
@@ -41,9 +56,6 @@
         "extensions.zotfile.authors_delimiter" = "_";
         "extensions.zotfile.removeDiacritics" = false;
         "extensions.zotfile.removePeriods" = false;
-
-        "extensions.zotero.translators.better-bibtex.citekeyFormat" = "auth.lower+shorttitle(3,3)+year";
-        "extensions.zotero.translators.better-bibtex.citekeyFormatEditing" = "auth.lower+shorttitle(3,3)+year";
       };
     };
   };
