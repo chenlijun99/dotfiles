0b03181b595249a19fc376857e22cc29a2008587
Lijun Chen <chenlijun1999@gmail.com>
2024-08-25 15:40:42 +0200

    vim: fine tune indent-blankline plugin settings

--- src/vim/config/plugin/indent-blankline.lua
+++ src/vim/config/plugin/indent-blankline.lua
@@ -5,7 +5,13 @@ return {
 		lazy = true,
 		event = "LazyFile",
 		config = function()
-			require("ibl").setup({})
+			require("ibl").setup({
+				scope = {
+					-- Don't show underlines. It's just confusing
+					show_start = false,
+					show_end = false,
+				}
+			})
 		end,
 	},
 }
