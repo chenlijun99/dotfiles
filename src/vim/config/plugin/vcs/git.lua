073fc165ca71f588467219fee843948b9188f45f
Lijun Chen <chenlijun1999@gmail.com>
2024-11-01 13:52:53 +0100

    vim: add some git-related mappings

--- src/vim/config/plugin/vcs/git.lua
+++ src/vim/config/plugin/vcs/git.lua
@@ -12,6 +12,39 @@ return {
 				changedelete = { text = "▎" },
 				untracked = { text = "▎" },
 			},
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				local function opts(desc)
+					return {
+						desc = "LSP: " .. desc,
+						noremap = true,
+					}
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next", { target = "all" })
+					end
+				end, opts("Next Git diff chunk"))
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev", { target = "all" })
+					end
+				end, opts("Previous Git diff chunk"))
+			end,
 		},
 	},
 }
