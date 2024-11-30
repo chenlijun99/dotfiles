5729357965ec9e3b41b444004632878572ede07f
Lijun Chen <chenlijun1999@gmail.com>
2024-11-30 11:40:58 +0100

    vim: add aerial mapping to fuzzy find symbols in buffer

--- src/vim/config/plugin/panel/aerial.lua
+++ src/vim/config/plugin/panel/aerial.lua
@@ -13,10 +13,26 @@ return {
 				"<cmd>AerialToggle!<CR>",
 				desc = "Symbols outline",
 			},
+			{
+				"<leader>fs",
+				"<cmd>call aerial#fzf()<cr>",
+				desc = "Fuzzy file symbols",
+			},
 		},
-		opts = { nav = {
-			preview = true,
-		} },
+		opts = {
+			nav = {
+				preview = true,
+			},
+			layout = {
+				-- These control the width of the aerial window.
+				-- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+				-- min_width and max_width can be a list of mixed types.
+				-- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+				max_width = { 50, 0.5 },
+				width = nil,
+				min_width = 10,
+			},
+		},
 		config = function(_, opts)
 			local aerial = require("aerial")
 			aerial.setup(opts)
