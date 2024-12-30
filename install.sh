611db1aef68c63363aa73ae977d19ac3781d32aa
Lijun Chen <chenlijun1999@gmail.com>
2024-12-30 21:15:28 +0100

    setup: remove old rofi config installation

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ function main()
 		echo "Pulling git submodules..."
 		git submodule update --init
 
-		echo "Installing rofi fonts..."
-		# rofi widget fonts
-		cp -n -r ./src/config/rofi/rofi/fonts/* ~/.local/share/fonts/
-
 		echo "Installing config files..."
 		# ensure that $BACKUP_DIR exists
 		mkdir -p "$BACKUP_DIR"
