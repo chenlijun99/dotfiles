29a4b7dc2818103c522fec21ae439ffecf414a52
Lijun Chen <chenlijun1999@gmail.com>
2025-02-24 20:45:39 +0100

    vim: add a few utility mappings to fzf file picker

--- src/vim/config/plugin/find/fzf.lua
+++ src/vim/config/plugin/find/fzf.lua
@@ -1,7 +1,10 @@
 return {
 	"ibhagwan/fzf-lua",
 	-- optional for icon support
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-lua/plenary.nvim",
+	},
 	keys = {
 		{
 			"<c-p>",
@@ -76,21 +79,91 @@ return {
 			desc = "Workspace symbols",
 		},
 	},
-	lazy = true,
-	opts = {
-		winopts = {
-			preview = {
-				layout = "vertical",
+	lazy = false,
+	config = function()
+		local file_copy_path_relative_to_cwd = function(selected, opts)
+			local path = require("fzf-lua").path
+			local entry = path.entry_to_file(selected[1], opts)
+			vim.fn.setreg("+", entry.path)
+			print("Copied path: " .. entry.path)
+		end
+
+		local file_copy_path_relative_to_current_buffer = function(
+			selected,
+			opts
+		)
+			local Path = require("plenary.path")
+
+			-- Copied from https://github.com/jhonasiv/workwork/pull/5/files
+			local function relative_path(original_path, reference_path)
+				-- Use plenary's make relative to clean paths
+				original_path = Path:new(original_path):make_relative(".")
+				reference_path = Path:new(reference_path):make_relative(".")
+				local path = Path:new(original_path)
+				local ref_path = Path:new(reference_path)
+				local parents = path:parents()
+				local ref_parents = ref_path:parents()
+
+				local path_elements = vim.split(path.filename, "/")
+				table.insert(parents, 1, original_path)
+				table.insert(ref_parents, 1, reference_path)
+
+				local result = ""
+				for i, ref_parent in ipairs(ref_parents) do
+					for j, par in ipairs(parents) do
+						if ref_parent == par then
+							if i == 1 and j == 1 then
+								return ""
+							end
+
+							result = result
+								.. table.concat(
+									path_elements,
+									"/",
+									#path_elements - j + 2
+								)
+							return result
+						end
+					end
+
+					result = "../" .. result
+				end
+			end
+
+			local path = require("fzf-lua").path
+			local entry = path.entry_to_file(selected[1], opts)
+			local rel_path = relative_path(
+				entry.path,
+				Path:new(vim.api.nvim_buf_get_name(0)):parent()
+			)
+			vim.fn.setreg("+", rel_path)
+			print("Copied path: " .. rel_path)
+		end
+
+		local opts = {
+			winopts = {
+				preview = {
+					layout = "vertical",
+				},
 			},
-		},
-		keymap = {
-			fzf = {
-				["ctrl-N"] = "next-history",
-				["ctrl-P"] = "prev-history",
+			keymap = {
+				fzf = {
+					["ctrl-N"] = "next-history",
+					["ctrl-P"] = "prev-history",
+				},
 			},
-		},
-		fzf_opts = {
-			["--history"] = vim.fn.stdpath("data") .. "/fzf-history",
-		},
-	},
+			actions = {
+				files = {
+					-- inherit from default config
+					true,
+					["ctrl-y"] = file_copy_path_relative_to_cwd,
+					["ctrl-r"] = file_copy_path_relative_to_current_buffer,
+				},
+			},
+			fzf_opts = {
+				["--history"] = vim.fn.stdpath("data") .. "/fzf-history",
+			},
+		}
+		require("fzf-lua").setup(opts)
+	end,
 }
