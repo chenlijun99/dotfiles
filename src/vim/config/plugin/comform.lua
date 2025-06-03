fa3fe30a9e45a33d5f5c9f3f77b15785b002116f
Lijun Chen <chenlijun1999@gmail.com>
2025-06-03 13:28:11 +0200

    vim: support formatting only changes since last commit

--- src/vim/config/plugin/comform.lua
+++ src/vim/config/plugin/comform.lua
@@ -1,3 +1,33 @@
+-- Taken from https://github.com/stevearc/conform.nvim/issues/92#issuecomment-2077222348
+function format_diff()
+	local ignore_filetypes = { "lua" }
+	if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+		vim.notify(
+			"range formatting for "
+				.. vim.bo.filetype
+				.. " not working properly."
+		)
+		return
+	end
+
+	local hunks = require("gitsigns").get_hunks()
+	local format = require("conform").format
+	for i = #hunks, 1, -1 do
+		local hunk = hunks[i]
+		if hunk ~= nil and hunk.type ~= "delete" then
+			local start = hunk.added.start
+			local last = start + hunk.added.count
+			-- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+			local last_hunk_line =
+				vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+			local range = {
+				start = { start, 0 },
+				["end"] = { last - 1, last_hunk_line:len() },
+			}
+			format({ range = range })
+		end
+	end
+end
 return {
 	{
 		"stevearc/conform.nvim",
@@ -14,6 +44,14 @@ return {
 				end,
 				mode = { "n", "v" },
 				desc = "Conform: format",
+			},
+			{
+				"<leader>gf",
+				function()
+					format_diff()
+				end,
+				mode = { "n" },
+				desc = "Conform: format git changes",
 			},
 			{
 				"<leader>lF",
