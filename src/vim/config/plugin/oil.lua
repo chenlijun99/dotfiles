1ed2fd87cb7909f2fd6401f47605c86ef2f52193
Lijun Chen <chenlijun1999@gmail.com>
2025-09-06 13:03:19 +0200

    vim: add oil.nvim
    Useful for batch file system operations

---
+++ src/vim/config/plugin/oil.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			default_file_explorer = true,
+			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<CR>"] = "actions.select",
+				["<C-s>"] = { "actions.select", opts = { vertical = true } },
+				["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+				["<C-t>"] = { "actions.select", opts = { tab = true } },
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = { "actions.close", mode = "n" },
+				["<C-l>"] = "actions.refresh",
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["`"] = { "actions.cd", mode = "n" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["<localleader>s"] = { "actions.change_sort", mode = "n" },
+				["gx"] = "actions.open_external",
+				["<localleader>h"] = { "actions.toggle_hidden", mode = "n" },
+			},
+		},
+		cmd = "Oil",
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		-- But I still use it anyway, since I only rarely use oil.nvim
+		lazy = true,
+	},
+}
