9ac15cd26140c37b32188af7e6e905ced75a40db
Lijun Chen <chenlijun1999@gmail.com>
2026-01-24 17:56:19 +0100

    kanata: filter also rks70 in wired mode and fine tune nixos kanata module cnofig

--- src/nixos/modules/kanata.nix
+++ src/nixos/modules/kanata.nix
@@ -1,17 +1,17 @@
 #
 # System-wide configuration related to Kanata
 #
-{pkgs, ...}: {
-  services.kanata = {
+{...}: {
+  services.kanata = let
+    katataConfigDir = ../../kanata;
+  in {
     enable = true;
-    # NOTE: need to concatText because configFile copies only the file itself, not the files it includes.
-    # Kanata thus fails to start since the configuration file is incomplete.
     keyboards = {
       "normal" = {
-        configFile = pkgs.concatText "kanata-normal-config" [../../kanata/normal.kbd ../../kanata/common.kbd];
+        configFile = "${katataConfigDir}/normal.kbd";
       };
       "rks70" = {
-        configFile = pkgs.concatText "kanata-normal-config" [../../kanata/rks70.kbd ../../kanata/common.kbd];
+        configFile = "${katataConfigDir}/rks70.kbd";
       };
     };
   };
