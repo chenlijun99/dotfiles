f116f44add39d0f7549636e5db0b15d15b30a1dd
Lijun Chen <chenlijun1999@gmail.com>
2026-03-08 01:26:47 +0100

    nixos-dendritic: add alacritty

---
+++ src/nixos-dendritic/modules/programs/alacritty.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.homeManager.clj-alacritty = {
+    pkgs,
+    config,
+    lib,
+    ...
+  }: {
+    options.clj.programs.alacritty.enable = lib.mkEnableOption "alacritty terminal emulator" // {default = true;};
+
+    config = lib.mkIf config.clj.programs.alacritty.enable {
+      home.packages = with pkgs; [
+        alacritty
+        # Used for bell sound in Alacritty
+        # E.g. `echo '\a'`
+        xorg.xkbutils
+      ];
+      xdg.configFile = {
+        "alacritty" = {
+          source = config.lib.clj.linkDotfile "src/config/alacritty/";
+        };
+      };
+    };
+  };
+}
